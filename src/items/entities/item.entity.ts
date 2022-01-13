3ff428ac788e74a4480bf74b46c695afb50ec108
Hju Kneyck Flores <hjucode@gmail.com>
2022-01-13 14:34:47 +0800

    chore: specify item entity definition

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,16 @@
-export class Item {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Item {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  price: number;
+}
