50081b3de5e08b16cb255a2b8ee987bc5ac04eda
Hju Kneyck Flores <hjucode@gmail.com>
2022-01-19 14:43:01 +0800

    fix: include `ORDER_MANAGER` in OrdersModule context

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDER_MANAGER',
+        transport: Transport.TCP,
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
